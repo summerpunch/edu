7b42d024ce4ea6590796e6ffc314d99ddc9a355b
xiachong <88388037@suningcultural.com>
2019-03-16 15:48:02 +0800

    edu test

---
+++ edu-java/src/main/java/com/icode/edu/EduJavaApplication.java
@@ -0,0 +1,13 @@
+package com.icode.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduJavaApplication.class, args);
+	}
+
+}
